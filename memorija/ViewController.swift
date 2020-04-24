ad25d37c362c98a0747544ccf3d7c2cb16b9a244
Rade <rade.vignjevic@students.finki.ukim.mk>
2020-04-24 11:34:54 +0200

    Initial Commit

---
+++ memorija/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memorija
+//
+//  Created by Rade Vignjevic on 4/24/20.
+//  Copyright © 2020 Nikola Vignjevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
